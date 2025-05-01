cd8570e76301de6c75ae6a6b1464e751c8dd9e80
Matheus Coelho <matheuscoelho060@gmail.com>
2025-05-01 10:55:45 -0300

    style: formmater on config files

--- .lintstagedrc.ts
+++ .lintstagedrc.ts
@@ -1,10 +1,10 @@
 module.exports = {
-    // Lint & Prettify TS and JS files
-    '**/*.(ts|tsx|js|jsx)': (filenames) => [
-      `eslint --fix ${filenames.join(' ')}`,
-      `prettier --write ${filenames.join(' ')}`,
-    ],
-  
-    // Prettify only Markdown and JSON files
-    '**/*.(md|json)': (filenames) => `prettier --write ${filenames.join(' ')}`,
-  }
+  // Lint & Prettify TS and JS files
+  '**/*.(ts|tsx|js|jsx)': (filenames) => [
+    `eslint --fix ${filenames.join(' ')}`,
+    `prettier --write ${filenames.join(' ')}`,
+  ],
+
+  // Prettify only Markdown and JSON files
+  '**/*.(md|json)': (filenames) => `prettier --write ${filenames.join(' ')}`,
+};
--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images:{
+  images: {
     domains: ['cubos-challenge.s3.us-east-2.amazonaws.com'],
-  }
+  },
 };
 
 export default nextConfig;
